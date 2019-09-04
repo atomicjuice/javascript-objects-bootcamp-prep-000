19f43a46153d2a148db9a3b053b420229c6fe8e5
keemo <keemowatson1@gmail.com>
2019-09-04 14:05:49 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,17 @@
+<<<<<<< HEAD
 var playlist = Object
 Object = {'artistName':'songTitle'}
 function updatePlaylist (playlist, artistName, songTitle){
   return Object.assign({Object},{name:song})
 }
+=======
+
+
+
+
+
+playlist={artistName:songTitle}
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({object},{name:song})
+}
+>>>>>>> 9375d93a5dc50cafc425c54b6bf41dd1ad39d593
